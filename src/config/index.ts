d4424bf660a8965d76db9ec3ed19c8cdcae99910
flawstick <amirashak101@gmail.com>
2024-04-18 09:10:03 +0300

    Fixing wrong URI format

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,8 +6,9 @@ export const config = {
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "jwt-secret-key",
   mongoUri:
-    `mongodb://${process.env.MONGODB_ADMIN_USERNAME}:${process.env.MONGODB_ADMIN_PASSWORD}@${process.env.MONGODB_URI}` ||
-    `mongodb://localhost:27017/${process.env.MONGODB_DB_NAME || "weaviate"}`,
+    `mongodb://${process.env.MONGODB_ADMIN_USERNAME}:${
+      process.env.MONGODB_ADMIN_PASSWORD
+    }@${process.env.MONGODB_URI || "localhost:27017"}` || "weaviate",
   mongoOptions: { dbName: process.env.MONGODB_DB_NAME || "weaviate" },
   weaviateUri: process.env.WEAVIATE_URI || "http://localhost:8080",
 };
