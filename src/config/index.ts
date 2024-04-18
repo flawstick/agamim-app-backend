f29bf36de37c07c51434e38616b14b94009b4445
flawstick <amirashak101@gmail.com>
2024-04-18 08:54:04 +0300

    Settings default container paths

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,7 +7,7 @@ export const config = {
   jwtSecret: process.env.JWT_SECRET || "jwt-secret-key",
   mongoUri:
     `mongodb://${process.env.MONGODB_ADMIN_USERNAME}:${process.env.MONGODB_ADMIN_PASSWORD}@${process.env.MONGODB_URI}` ||
-    "mongodb://localhost:27017/weaviate",
+    `mongodb://localhost:27017/${process.env.MONGODB_DB_NAME || "weaviate"}`,
   mongoOptions: { dbName: process.env.MONGODB_DB_NAME || "weaviate" },
   weaviateUri: process.env.WEAVIATE_URI || "http://localhost:8080",
 };
