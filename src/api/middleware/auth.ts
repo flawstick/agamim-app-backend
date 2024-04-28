52219a03859dc6e919db529a7de5b645c58f8ab6
flawstick <amirashak101@gmail.com>
2024-04-28 21:55:36 +0300

    Fixing header names

--- src/api/middleware/auth.ts
+++ src/api/middleware/auth.ts
@@ -3,7 +3,7 @@ import { config } from "@/config";
 
 export const verifyJsonWebToken = (req: any, res: any, next: any) => {
   if (req.path === "/auth/login") return next();
-  const token = req.headers.Authorization?.split(" ")[1]; // Bearer token
+  const token = req.headers.authorization?.split(" ")[1]; // Bearer token
 
   if (!token) return res.status(401).json({ message: "No token provided" });
 
