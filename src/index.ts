e2c6f2a9bbdcd2810a353268788e19db967303c8
flawstick <amirashak101@gmail.com>
2024-06-16 02:55:53 +0300

    Any origin can acccess the API.

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000", // Replace with your frontend origin
+    origin: "*",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     allowedHeaders: "Content-Type,Authorization",
   }),
