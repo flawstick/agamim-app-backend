623ccec60811edebaf92c3f76e13607bcb25babf
flawstick <amirashak101@gmail.com>
2024-07-01 00:12:06 +0300

    Allowing tenant id in headers

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin: "*",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    allowedHeaders: "Content-Type,Authorization",
+    allowedHeaders: "Content-Type,Authorization,x-tenant-id",
   }),
 );
 
