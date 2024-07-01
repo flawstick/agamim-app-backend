bd37f9ad96afe58d816c89d653755664f6dbb8cf
flawstick <amirashak101@gmail.com>
2024-07-01 19:13:58 +0300

    Adding password change functionality

--- src/api/controllers/userController.ts
+++ src/api/controllers/userController.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import { validationResult } from "express-validator";
 import UserModel from "@/models/user";
 import CompanyModel from "@/models/company";
+import { hashPassword } from "@/utils/bcrypt";
 import { log } from "@/utils/log";
 
 export const getUsersByTenantId = async (req: Request, res: Response) => {
@@ -91,17 +92,23 @@ export const createUser = async (req: Request, res: Response) => {
 
   try {
     const company = await CompanyModel.findOne({ tenantId: user.tenantId });
-    if (!company || !company.members?.includes(userId)) {
+    if (
+      !company ||
+      !company.members?.some((member) => member.toString() === userId)
+    ) {
       log.warn(
         `User with ID ${userId} not authorized to create users in tenant ID ${user.tenantId}`,
       );
       return res.status(403).json({ message: "User not authorized" });
     }
 
-    const newUser = new UserModel(req.body);
+    // Hash the user's password before saving
+    const hashedPassword = await hashPassword(user.hashedPassword, 10);
+    const newUser = new UserModel({ ...user, hashedPassword });
+
     await newUser.save();
     log.info("Created new user");
-    res.status(201).json({ ...newUser, user: null });
+    res.status(201).json({ ...newUser.toObject(), user: null });
   } catch (error) {
     log.error("Error creating user:", error as Error);
     res.status(500).json({ message: "Error creating user", error });
@@ -149,13 +156,16 @@ export const updateUser = async (req: Request, res: Response) => {
 export const deleteUser = async (req: Request, res: Response) => {
   const { id } = req.params;
   const tenantId = req.headers["x-tenant-id"] as string;
-  const userId = req.body.user.userId;
+  const accountId = req.body.user.userId;
 
   try {
     const company = await CompanyModel.findOne({ tenantId });
-    if (!company || !company.members?.includes(userId)) {
+    if (
+      !company ||
+      !company.members?.some((member) => member.toString() === accountId)
+    ) {
       log.warn(
-        `User with ID ${userId} not authorized to delete users in tenant ID ${tenantId}`,
+        `User with ID ${accountId} not authorized to create users in tenant ID ${tenantId}`,
       );
       return res.status(403).json({ message: "User not authorized" });
     }
@@ -172,5 +182,53 @@ export const deleteUser = async (req: Request, res: Response) => {
   } catch (error) {
     log.error(`Error deleting user with ID ${id}:`, error as Error);
     res.status(500).json({ message: "Error deleting user", error });
+  }
+};
+
+export const changePassword = async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    log.warn("Validation errors:", errors.array());
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const tenantId = req.headers["x-tenant-id"] as string;
+  const { userId, newPassword, confirmPassword } = req.body;
+  const accountId = req.body.user.userId;
+
+  if (newPassword !== confirmPassword) {
+    log.warn(`Password confirmation does not match.`);
+    return res
+      .status(400)
+      .json({ message: "New password and confirmation do not match" });
+  }
+
+  try {
+    const company = await CompanyModel.findOne({ tenantId });
+    if (
+      !company ||
+      !company.members?.some((member) => member.toString() === accountId)
+    ) {
+      log.warn(
+        `User with ID ${accountId} not authorized to create users in tenant ID ${tenantId}`,
+      );
+      return res.status(403).json({ message: "User not authorized" });
+    }
+
+    const user = await UserModel.findById(userId);
+    if (!user) {
+      log.warn(`User with ID ${userId} not found`);
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const hashedPassword = await hashPassword(newPassword);
+    user.hashedPassword = hashedPassword;
+    await user.save();
+
+    log.info(`Password updated for user ID ${userId}`);
+    res.status(200).json({ message: "Password updated successfully" });
+  } catch (error) {
+    log.error(`Error changing password for user ID ${userId}:`, error as Error);
+    res.status(500).json({ message: "Error changing password", error });
   }
 };
--- src/api/routes/userRoutes.ts
+++ src/api/routes/userRoutes.ts
@@ -1,11 +1,12 @@
 import express from "express";
-import { check } from "express-validator";
+import { check, body } from "express-validator";
 import {
   getUsersByTenantId,
   getUserById,
   createUser,
   updateUser,
   deleteUser,
+  changePassword,
 } from "@/api/controllers/userController";
 
 const router = express.Router();
@@ -24,10 +25,21 @@ const validateUser = [
   check("tenantId").notEmpty().withMessage("Tenant ID is required"),
 ];
 
+export const validateChangePassword = [
+  body("newPassword")
+    .isLength({ min: 6 })
+    .withMessage("New password must be at least 6 characters long"),
+  body("confirmPassword")
+    .notEmpty()
+    .withMessage("Password confirmation is required"),
+  body("userId").notEmpty().withMessage("User ID is required"),
+];
+
 router.get("/", getUsersByTenantId);
 router.get("/:id", getUserById);
 router.post("/", validateUser, createUser);
 router.put("/:id", validateUser, updateUser);
 router.delete("/:id", deleteUser);
+router.post("/change-password", validateChangePassword, changePassword);
 
 export default router;
