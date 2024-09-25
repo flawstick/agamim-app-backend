a35e6a76a42540598066683788ce3b13f127346e
flawstick <amirashak101@gmail.com>
2024-09-25 10:12:53 +0300

    Adding tenantId to user login

--- src/api/controllers/login.ts
+++ src/api/controllers/login.ts
@@ -32,6 +32,7 @@ export default async function loginUser(req: any, res: any) {
       token: token,
       userData: {
         ...user,
+        tenantId,
         username: undefined,
         hashedPassword: undefined,
         createdAt: undefined,
