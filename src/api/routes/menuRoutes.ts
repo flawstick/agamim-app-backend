8b24fbb204c83a2b77543e4500e01af1034f6de6
flawstick <amirashak101@gmail.com>
2024-11-04 07:16:01 +0200

    Fixing lint error

--- src/api/routes/menuRoutes.ts
+++ src/api/routes/menuRoutes.ts
@@ -4,14 +4,12 @@ import {
   createModifier,
   deleteModifier,
   fetchModifiers,
-  getAllMenuItems,
   getItemsAndCategories,
   putModifier,
 } from "@/api/controllers/menuController";
 
 const router = express.Router();
 
-router.get("/allItems", getAllMenuItems);
 router.get("/:restaurantId/modifiers/", authenticateUser, fetchModifiers);
 router.post("/:restaurantId/modifiers/", authenticateUser, createModifier);
 router.put("/:restaurantId/modifiers/:mId", authenticateUser, putModifier);
