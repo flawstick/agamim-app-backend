d69aafc7b631bdaecce11a8518a5780006e2c9f3
flawstick <amirashak101@gmail.com>
2024-11-04 04:09:01 +0200

    Adding cast, maybe that works

--- src/menu/menuLink.ts
+++ src/menu/menuLink.ts
@@ -17,7 +17,7 @@ export async function linkMenuToRestaurant(
     const restaurant = await RestaurantModel.findOne({ _id: restaurantId });
     if (!restaurant) throw new Error("Restaurant or menu not found");
 
-    if (restaurant?.menu?.toString() === menuId) return;
+    if (restaurant?.menu?.cast?.toString()  === menuId) return;
     restaurant.menu = new mongoose.Types.ObjectId(menuId) as any;
     await restaurant.save();
     log.info(`Linked menu ${menuId} to restaurant ${restaurantId}`);
