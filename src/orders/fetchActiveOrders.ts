59642589cabcf0853f781fff69479b0df2c3862a
flawstick <amirashak101@gmail.com>
2024-11-20 03:35:45 +0200

    minor

--- src/orders/fetchActiveOrders.ts
+++ src/orders/fetchActiveOrders.ts
@@ -17,19 +17,19 @@ export async function fetchActiveOrders(
   userId: Types.ObjectId,
   tenantId: string,
 ): Promise<any> {
+  console.log("fetchActiveOrders");
+  console.log("userId: ", userId);
+  console.log("tenantId: ", tenantId);
   const user = await UserModel.findOne({
     _id: userId,
     tenantId: tenantId,
   });
-  if (!user) return { message: "User not found" };
-  console.log("User found");
-  console.log(userId);
-  console.log(tenantId);
+  if (!user) throw new Error("User not found");
 
   const company = await CompanyModel.findOne({
     tenantId,
   });
-  if (!company) return { message: "Company not found" };
+  if (!company) throw new Error("Company not found");
 
   // find orders that have status "pending" or "confirmed"
   let orders = await OrderModel.find({
@@ -37,5 +37,5 @@ export async function fetchActiveOrders(
     status: { $in: ["pending", "confirmed"] },
   });
 
-  return orders || { message: "No Orders." };
+  return orders || [];
 }
